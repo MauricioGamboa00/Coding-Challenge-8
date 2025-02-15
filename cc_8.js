9e10ecff3d51f423502ca1acaf6f344a02d82dc9
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-15 15:25:25 -0500

    Task 8 - Savings Growth Projection

--- cc_8.js
+++ cc_8.js
@@ -84,3 +84,15 @@ let cart = createCartTracker(); // create a new instane of cart tracker
 console.log(cart(20)); // Expected output: "Total Cart Value: $20"
 console.log(cart(35)); // Expected output: "Total Cart Value: $55"
 
+// Task 8 Recursion in JavaScript
+
+function calculateSavings(years, amount) {
+    if (years >=10) {
+        return `Projected Saving: $${amount.toFixed(2)}`; // return formatted projected savings until it reaches 10 or more years
+    };
+
+    return calculateSavings(years + 1, amount * 1.05); // Increase saving by 5% for each year
+};
+
+console.log(calculateSavings(8, 1000)); // Expected output: "Projected Savings: $1102.50"
+console.log(calculateSavings(5, 5000)); // Expected output: "Projected Savings: $6381.41"
